19c257e65a7adfa0f2cedecc484a88bd9c37bab8
kingsman20 <kingslifeudo@gmail.com>
2020-05-25 23:41:21 +0100

    Completed the section on using a database system

--- index.js
+++ index.js
@@ -1,84 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const houses = require('./routes/houses');
 
 const app = express();
 
 app.use(express.json());
 
-require('dotenv').config();
-
-const homes = [
-    {
-        id: 1,
-        type: 'Apartment',
-        description: 'Well furnished apartment'
-    },
-    {
-        id: 2,
-        type: 'Flat',
-        description: 'Well furnished flat at flatville'
-    }
-]
-
 app.get('/', (req, res) => {
-    res.send('Welcome to express');
+    res.send('Welcome to the house listing API');
 });
 
-app.get('/api/listing', (req, res) => {
-    res.send(homes);
-})
+app.use('/api/houses', houses);
 
-app.get('/api/listing/:id', (req, res) => {
-    const home = homes.find(home => home.id === parseInt(req.params.id));
-
-    if(!home) {
-        res.status(404).send('The home with given ID is not found')
-    }
-
-    res.send(home);
-})
-
-app.post('/api/listing', (req, res) => {
-
-    if(!req.body.type || !req.body.description) {
-        return res.status(400).send('Title and description is required')
-    }
-
-    const home = {
-        id: homes.length + 1,
-        type: req.body.type,
-        description: req.body.description,
-    }
-
-    homes.push(home);
-    res.send(home);
-})
-
-app.put('/api/listing/:id', (req, res) => {
-    const home = homes.find(home => home.id === parseInt(req.params.id));
-
-    if(!home) {
-        return res.status(404).send('The home with given ID is not found')
-    }
-
-    home.type = req.body.type;
-    home.description = req.body.description;
-
-    res.send(home);
-    
-})
-
-app.delete('/api/listing/:id', (req, res) => {
-    const home = homes.find(home => home.id === parseInt(req.params.id));
-
-    if(!home) {
-        return res.status(404).send('The home with given ID is not found')
-    }
-
-    const index = homes.indexOf(home);
-    homes.splice(index, 1);
-    res.send(home);
-})
+require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+mongoose.connect('mongodb+srv://kingslife_node_react-native_1:DROP92qoHBR2yG4W@cluster0-2f3l4.mongodb.net/house_app?retryWrites=true&w=majority')
+    .then(result => {
+        app.listen(port, () => console.log(`Server is running on port ${port}`))
+    })
+    .catch(err => console.log(err))
